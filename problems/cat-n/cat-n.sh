8b3585809652a224e6460a63a9631fd3e3561938
Matthew Hufford <mhufford@login3.cm.cluster>
2017-09-29 06:32:04 -0700

    did we not have to do head.sh?  oops

--- problems/cat-n/cat-n.sh
+++ problems/cat-n/cat-n.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-echo "OK"
+number=1;
+
+if [[ $# == 0 ]]; then
+	echo "Usage: cat-n.sh file\n)"
+        exit 1
+
+fi
+
+if [[ -f ${1:-'srr.txt'} ]]; then
+
+FILE=$1
+number=1
+
+	while read -r LINE; do
+		echo "$number $LINE"
+		let "number++"
+	done < "$FILE"
+else
+	printf "\"${1:-'srr.txt'}\" is not a file\n)"
+	exit 1
+fi
+
