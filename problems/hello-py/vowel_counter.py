bfd82558091140900e40ff99c322b77196786f64
Matthew Hufford <mhufford@login3.cm.cluster>
2017-10-05 12:20:25 -0700

    homework

--- problems/hello-py/vowel_counter.py
+++ problems/hello-py/vowel_counter.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
-"""count the vowels in a word"""
+from collections import Counter
+import sys, string
+import os
 
-print('OK')
+args = sys.argv
+
+if len(args) < 2:
+        script = os.path.basename(args[0])
+        print('Usage: vowel_counter.py STRING'.format(script))
+        sys.exit(1)
+
+word = args[1]
+numVowels = 0
+#vowels = list('a,e,i,o,u')
+vowels = 'aeiou'
+
+for char in word:
+        if char in vowels:
+                numVowels += 1
+
+verb = 'is ' if numVowels == 1 else 'are '
+noun = 'vowel ' if numVowels == 1 else 'vowels '
+print('There ' + verb + str(numVowels) + ' ' +  noun + 'in "' + word + '."')
