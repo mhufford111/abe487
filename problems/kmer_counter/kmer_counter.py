2b3140917da0980290078fec79cf4c11b264863c
Matthew Hufford <mhufford@login3.cm.cluster>
2017-10-19 13:48:16 -0700

    working!

--- problems/kmer_counter/kmer_counter.py
+++ problems/kmer_counter/kmer_counter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+# Kmer counter
 
 from collections import Counter
-from collections import defaultdict
 import sys, string
 import os
 import os.path
@@ -9,48 +9,47 @@ from pathlib import Path
 
 args = sys.argv[1:]
 if len(args) < 2:
-        print("Usage: kmer_counter.py LEN STR")
-        sys.exit(1)
+	print("Usage: kmer_counter.py LEN STR")
+	sys.exit(1)
 
 if args[0].isdigit():
-        kmer_size = int(args[0])
+	kmer_size = int(args[0])
 else:
-        print('Kmer size "{}" is not a number'.format(args[0]))
-        sys.exit(1)
+	print('Kmer size "{}" is not a number'.format(args[0]))
+	sys.exit(1)
 
-if(kmer_size<=0):
-        print('Kmer size "{}" must be > 0'.format(kmer_size))
+if kmer_size <= 0:
+	print('Kmer size "{}" must be > 0'.format(kmer_size))
 	sys.exit(1)
 
 input = args[1]
-
 nkmers = len(input) - kmer_size + 1
 
 if kmer_size > len(input):
-	print('There are no () mers in "{}"'.format(kmer_size, args[1]))
+	print('There are no {}-mers in "{}"'.format(kmer_size, args[1]))
 	sys.exit(1)
 
-kmers = {}
+kmer = {}
 
 i = 0
 add = ""
 j = 0
-z = 0
+k = 0
 
 while i < nkmers:
 	add = ""
 	j = 0
-	z = i
+	k = i
 	while j < kmer_size:
-		add += input[z+j]
+		add += input[k+j]
 		j += 1
-	if add in kmers:
-		kmers[add] += 1
+	if add in kmer:
+		kmer[add] += 1
 	else:
-		kmers[add] = 1
+		kmer[add] = 1
 	i += 1
 
 print('{}'.format(input))
 
-for x in sorted(kmers):
-	print(x, kmers[x])
+for x in sorted(kmer):
+	print(x, kmer[x])
