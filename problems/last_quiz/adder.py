a82168d87613818d45a18e7c36a5c5b20cc3a7f4
Matthew Hufford <mhufford@login2.cm.cluster>
2017-11-30 12:54:16 -0700

    last quiz

--- problems/last_quiz/adder.py
+++ problems/last_quiz/adder.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
+import os
+import sys
 
-print('OK')
+args = sys.argv[1:]
+
+if len(args) != 2:
+    print('usage')
+    sys.exit(1)
+
+x = args[0]
+y = args[1]
+
+if x.isdigit() and y.isdigit():
+    z = int(x) + int(y)
+    print(z)
+elif not x.isdigit() and not y.isdigit():
+    print(x + ' and ' + y)
+
+else:
+    print('Cannot combine number and string')
+    exit(1)
+
